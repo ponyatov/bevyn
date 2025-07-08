51c494fa0efc9138bb434c395c7c30305912f46e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-08 15:29:16 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() { println!("Hello, world!"); }
+// https://bevy.org/learn/quick-start/getting-started/apps/
+
+use bevy::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+    App::new().run();
+}
